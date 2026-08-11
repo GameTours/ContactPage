7e28227abc95adefb9b8759e1298a11f67e96ab6
GameTours <joe.young@sky.com>
2026-08-11 19:22:25 +0100

    Cleaned up the site files and structure.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     site: 'https://jtyoung.me',
-    integrations: [mdx(), sitemap()],
+    integrations: [sitemap()],
     vite: {
         plugins: [tailwindcss()],
         ssr: {
